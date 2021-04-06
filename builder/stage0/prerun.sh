d6c7d1d664d1f6d31e83404d465183f63cb18f79
bclswl0827 <24319042+bclswl0827@users.noreply.github.com>
2021-04-06 13:24:43 +0800

    Use ustc instead of bfsu.

--- builder/stage0/prerun.sh
+++ builder/stage0/prerun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
-	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://mirrors.bfsu.edu.cn/raspbian/raspbian/
+	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://mirrors.ustc.edu.cn/raspbian/raspbian/
 fi
